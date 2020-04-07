aecfe9cd56a921f294fc2a598f97a463330ab4eb
ukkey0518 <ukitto0518@gmail.com>
2020-04-07 17:28:09 +0900

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('顧客管理アプリ'),
+      ),
+    );
+  }
+}
