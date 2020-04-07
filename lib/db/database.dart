31e3cfca31d787a694721e59731b378377945815
ukkey0518 <ukitto0518@gmail.com>
2020-04-07 20:18:30 +0900

    [database] コネクション生成関数 作成
    [_openConnection()]
    ・初回アクセス時、DBファイル作成
    ・次回以降は同じファイルにアクセスする

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:moor/moor.dart';
+import 'package:moor_ffi/moor_ffi.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 class Customers extends Table {
   IntColumn get id => integer().autoIncrement()();
@@ -8,6 +13,14 @@ class Customers extends Table {
 
   @override
   Set<Column> get primaryKey => {id};
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'customer.db'));
+    return VmDatabase(file);
+  });
 }
 
 @UseMoor(tables: [Customers])
