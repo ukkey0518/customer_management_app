0e4f2f7df54338bf285d062ffdab83fc55f59fb3
ukkey0518 <ukitto0518@gmail.com>
2020-04-07 21:26:05 +0900

    [プロジェクト] 編集画面 追加
    [edit_screen]
    ・顧客情報を編集または登録する画面
    ・内容は未実装

---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class EditScreen extends StatefulWidget {
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
