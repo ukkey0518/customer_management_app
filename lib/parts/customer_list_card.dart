f8e1795dc78f38f5cac7b64c040ec77659810b2c
ukkey0518 <ukitto0518@gmail.com>
2020-04-10 12:03:19 +0900

    [customer_lost_card] 修正：ListTileから独自レイアウトへ変更
    [CustomerListCard]
    ・ListTileを削除、独自レイアウトを作成
    ・InkWellでラップし、タップコールバック付与
    ・アイコンは性別によって色が変わるように修正

--- lib/parts/customer_list_card.dart
+++ lib/parts/customer_list_card.dart
@@ -2,32 +2,59 @@ import 'package:customermanagementapp/db/database.dart';
 import 'package:flutter/material.dart';
 
 class CustomerListCard extends StatelessWidget {
+  CustomerListCard({this.customer, this.onTap, this.onLongPress});
+
   final Customer customer;
-
   final GestureTapCallback onTap;
-
   final GestureLongPressCallback onLongPress;
 
-  CustomerListCard({this.customer, this.onTap, this.onLongPress});
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: SizedBox(
-          height: double.infinity,
-          child: Icon(Icons.account_circle),
-        ),
-        title: Text(
-          '${customer.name}',
-          style: TextStyle(fontSize: 20),
-        ),
-        subtitle: Text(
-          '${customer.nameReading}',
-          style: TextStyle(fontSize: 16),
-        ),
+      child: InkWell(
         onTap: onTap,
         onLongPress: onLongPress,
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: _getGenderIcon(),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${customer.nameReading}',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          '${customer.name} 様',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _getGenderIcon() {
+    var iconColor =
+        customer.gender == '女性' ? Colors.pinkAccent : Colors.blueAccent;
+
+    return Icon(
+      Icons.account_circle,
+      color: iconColor,
     );
   }
 }
