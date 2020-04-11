909b6cc727a14f58ebbdf5947eb70fe3c22a5591
ukkey0518 <ukitto0518@gmail.com>
2020-04-11 14:08:34 +0900

    [プロジェクト] 追加：すべての来店記録を表示する画面を作成
    [visit_record_list_screen]

---
+++ lib/screens/visit_record_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VisitRecordListScreen extends StatefulWidget {
+  @override
+  _VisitRecordListScreenState createState() => _VisitRecordListScreenState();
+}
+
+class _VisitRecordListScreenState extends State<VisitRecordListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
