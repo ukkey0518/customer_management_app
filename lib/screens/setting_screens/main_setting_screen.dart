5f2391cacb5799fe51184a36e83591a3a3723ee0
ukkey0518 <ukitto0518@gmail.com>
2020-04-12 18:45:05 +0900

    [main_setting_screen] 追加：各メニューリスト項目
    [MainSettingScreen]
    ・メニューカテゴリ管理
    ・メニュー管理
    ・スタッフ管理
    項目を追加

--- lib/screens/setting_screens/main_setting_screen.dart
+++ lib/screens/setting_screens/main_setting_screen.dart
@@ -9,7 +9,28 @@ class MainSettingScreen extends StatelessWidget {
         title: const Text('各種設定画面'),
       ),
       drawer: MyDrawer(),
-      body: Container(),
+      body: ListView(
+        children: <Widget>[
+          Divider(height: 1),
+          _settingListItemBuilder(title: 'メニューカテゴリ管理', onTap: null),
+          Divider(height: 1),
+          _settingListItemBuilder(title: 'メニュー管理', onTap: null),
+          Divider(height: 1),
+          _settingListItemBuilder(title: 'スタッフ管理', onTap: null),
+        ],
+      ),
+    );
+  }
+
+  // [ウィジェットビルダー：リストアイテムのビルダー]
+  Widget _settingListItemBuilder(
+      {@required String title, @required VoidCallback onTap}) {
+    return ListTile(
+      contentPadding: EdgeInsets.only(top: 4, bottom: 4, left: 16, right: 8),
+      leading: Icon(Icons.settings),
+      trailing: Icon(Icons.chevron_right),
+      title: Text(title),
+      onTap: onTap,
     );
   }
 }
