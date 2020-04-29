aa9a1bf6a192eba9716e8336e140ff00127d0e6e
ukkey0518 <ukitto0518@gmail.com>
2020-04-29 17:15:58 +0900

    [CustomersListScreen] 修正：リスト部分をクラスとして分離

---
+++ lib/view/components/customers_list_view.dart
@@ -0,0 +1,32 @@
+import 'package:customermanagementapp/db/database.dart';
+import 'package:flutter/material.dart';
+
+import 'customer_list_card.dart';
+
+class CustomersListView extends StatelessWidget {
+  CustomersListView({this.customers, this.onItemTap, this.onItemLongPress});
+
+  final List<Customer> customers;
+  final ValueChanged<Customer> onItemTap;
+  final ValueChanged<Customer> onItemLongPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            var customer = customers[index];
+            return CustomerListCard(
+              customer: customer,
+              onTap: () => onItemTap(customer),
+              onLongPress: () => onItemLongPress(customer),
+            );
+          },
+          itemCount: customers.length,
+        ),
+      ),
+    );
+  }
+}
--- lib/view/screens/customers_list_screens/customers_list_screen.dart
+++ lib/view/screens/customers_list_screens/customers_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:customermanagementapp/db/database.dart';
 import 'package:customermanagementapp/list_status.dart';
 import 'package:customermanagementapp/main.dart';
-import 'package:customermanagementapp/view/components/customer_list_card.dart';
+import 'package:customermanagementapp/view/components/customers_list_view.dart';
 import 'package:customermanagementapp/view/components/my_drawer.dart';
 import 'package:customermanagementapp/view/components/search_bar.dart';
 import 'package:customermanagementapp/view/screens/customers_list_screens/customer_edit_screen.dart';
@@ -140,21 +140,10 @@ class _CustomersListScreenState extends State<CustomersListScreen> {
             ),
           ),
           Divider(),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  var customer = _customersList[index];
-                  return CustomerListCard(
-                    customer: customer,
-                    onTap: () => _showCustomer(customer),
-                    onLongPress: () => _deleteCustomer(customer),
-                  );
-                },
-                itemCount: _customersList.length,
-              ),
-            ),
+          CustomersListView(
+            customers: _customersList,
+            onItemTap: (customer) => _showCustomer(customer),
+            onItemLongPress: (customer) => _deleteCustomer(customer),
           ),
         ],
       ),
