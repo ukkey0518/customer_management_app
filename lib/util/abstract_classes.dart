abba4241025123447c16cc5cb02ede372f059b4a
ukkey0518 <ukitto0518@gmail.com>
2020-04-30 22:32:51 +0900

    InputWidgetMixInの削除
    ・抽象クラスでポリモーフィズムを実現しているので不要

--- lib/util/abstract_classes.dart
+++ lib/util/abstract_classes.dart
@@ -1,4 +1,3 @@
-import 'package:customermanagementapp/util/mixins.dart';
 import 'package:flutter/cupertino.dart';
 
-abstract class InputWidget extends StatelessWidget with InputWidgetMixIn {}
+abstract class InputWidget extends StatelessWidget {}
