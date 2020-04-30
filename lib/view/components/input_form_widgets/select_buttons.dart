5a293b4c68dfcd536e52288f3b7538352521e4ed
ukkey0518 <ukitto0518@gmail.com>
2020-04-30 20:50:13 +0900

    [SelectButtons] 修正：Padding追加

--- lib/view/components/input_form_widgets/select_buttons.dart
+++ lib/view/components/input_form_widgets/select_buttons.dart
@@ -17,17 +17,20 @@ class SelectButtons extends InputWidget {
     return Row(
       children: List<Widget>.generate(values.length, (index) {
         var value = values[index];
-        return RaisedButton(
-          color: value == selectedValue
-              ? Theme.of(context).primaryColorLight
-              : Colors.white,
-          child: Text(
-            value,
-            style: TextStyle(
-              color: value == selectedValue ? Colors.black : Colors.grey,
+        return Padding(
+          padding: const EdgeInsets.only(right: 4),
+          child: RaisedButton(
+            color: value == selectedValue
+                ? Theme.of(context).primaryColorLight
+                : Colors.white,
+            child: Text(
+              value,
+              style: TextStyle(
+                color: value == selectedValue ? Colors.black : Colors.grey,
+              ),
             ),
+            onPressed: () => onChanged(value),
           ),
-          onPressed: () => onChanged(value),
         );
       }),
     );
