82166fdb0d9654fce0337c49f173361c4ea41e5f
ukkey0518 <ukitto0518@gmail.com>
2020-05-02 18:28:15 +0900

    [BasicInformationPart] 修正：来店情報部分を別クラスとして定義
    [クラス：VisitInformationCardWidget]
    ・プロフィール部分を表示する

---
+++ lib/view/components/list_card_widgets/visit_information_card_widgt.dart
@@ -0,0 +1,21 @@
+import 'package:customermanagementapp/view/components/polymorphism/list_card_view.dart';
+import 'package:customermanagementapp/util/extensions.dart';
+
+import 'package:flutter/material.dart';
+
+class VisitInformationCardWidget extends ListCardWidget {
+  VisitInformationCardWidget({
+    @required int numberOfVisit,
+    @required DateTime firstVisitDate,
+    @required DateTime lastVisitDate,
+    @required String reasonForVisit,
+  }) : super(
+          title: '来店情報',
+          contentsMap: {
+            '来店回数': numberOfVisit.toString(),
+            '初回来店': firstVisitDate.toFormatString(),
+            '最終来店': lastVisitDate.toFormatString(),
+            '初回来店理由': reasonForVisit,
+          },
+        );
+}
