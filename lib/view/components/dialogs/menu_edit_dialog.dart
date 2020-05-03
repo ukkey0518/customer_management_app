8308da7e48683d5f7556e4819e3dd8691f30b419
ukkey0518 <ukitto0518@gmail.com>
2020-05-03 16:46:21 +0900

    [MenuEditDialog] 修正：入力欄をInputFieldへ変更
    ・エラーテキスト追加

--- lib/view/components/dialogs/menu_edit_dialog.dart
+++ lib/view/components/dialogs/menu_edit_dialog.dart
@@ -1,7 +1,8 @@
+import 'package:customermanagementapp/data/input_field_style.dart';
 import 'package:customermanagementapp/db/database.dart';
+import 'package:customermanagementapp/view/components/input_widgets/input_field.dart';
 
 import 'package:flutter/material.dart';
-import 'package:toast/toast.dart';
 
 class MenuEditDialog extends StatelessWidget {
   MenuEditDialog({this.category, this.menu});
@@ -14,9 +15,11 @@ class MenuEditDialog extends StatelessWidget {
     var nameController = TextEditingController();
     var priceController = TextEditingController();
 
+    var nameErrorText;
+    var priceErrorText;
+
     if (menu == null) {
       titleText = 'メニューの追加';
-
       nameController.text = '';
       priceController.text = '';
     } else {
@@ -34,71 +37,31 @@ class MenuEditDialog extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text('カテゴリ：'),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  child: TextField(
+                AbsorbPointer(
+                  absorbing: true,
+                  child: InputField(
                     controller: TextEditingController(text: category.name),
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      prefixIcon: Icon(
-                        Icons.category,
-                        color: Color(category.color),
-                      ),
-                    ),
+                    style: InputFieldStyle.ROUNDED_RECTANGLE,
+                    errorText: null,
                   ),
                 ),
                 SizedBox(height: 30),
                 Text('メニュー名：'),
-                Container(
-                  padding: EdgeInsets.only(left: 16),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  child: TextField(
-                    controller: nameController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: 'メニュー名を入力',
-                      border: InputBorder.none,
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () =>
-                            WidgetsBinding.instance.addPostFrameCallback(
-                          (_) => nameController.clear(),
-                        ),
-                      ),
-                    ),
-                  ),
+                InputField(
+                  controller: nameController,
+                  errorText: nameErrorText,
+                  style: InputFieldStyle.ROUNDED_RECTANGLE,
+                  hintText: 'メニュー名を入力',
+                  isClearable: true,
                 ),
                 SizedBox(height: 30),
                 Text('価格：'),
-                Container(
-                  padding: EdgeInsets.only(left: 16),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  child: TextField(
-                    controller: priceController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: '価格を入力',
-                      border: InputBorder.none,
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () =>
-                            WidgetsBinding.instance.addPostFrameCallback(
-                          (_) => priceController.clear(),
-                        ),
-                      ),
-                    ),
-                  ),
+                InputField(
+                  controller: priceController,
+                  errorText: priceErrorText,
+                  style: InputFieldStyle.ROUNDED_RECTANGLE,
+                  hintText: '価格を入力',
+                  isClearable: true,
                 ),
               ],
             ),
@@ -112,18 +75,20 @@ class MenuEditDialog extends StatelessWidget {
               child: const Text('追加'),
               onPressed: () {
                 // 未入力チェック
-                if (nameController.text.isEmpty ||
-                    priceController.text.isEmpty) {
-                  Toast.show('未入力項目があります', context);
-                  return;
-                }
+                nameErrorText = nameController.text.isEmpty ? '必須入力です' : null;
+                priceErrorText = priceController.text.isEmpty ? '必須入力です' : null;
 
                 // 数値チェック
                 try {
                   int.parse(priceController.text);
-                } on FormatException catch (e) {
-                  Toast.show('価格は数字のみ入力可能です。', context);
-                  print(e);
+                  priceErrorText = priceErrorText;
+                } on FormatException catch (_) {
+                  priceErrorText = '価格は数字のみ入力可能です';
+                }
+
+                setState(() {});
+
+                if (nameErrorText != null || priceErrorText != null) {
                   return;
                 }
 
