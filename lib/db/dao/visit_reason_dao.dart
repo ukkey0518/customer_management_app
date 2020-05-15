62e344bb0e3439bcffa137e90c15e5dea07e4746
ukkey0518 <ukitto0518@gmail.com>
2020-05-15 13:09:38 +0900

    [DAO] 追加：VisitReasonsDaoの各処理を定義

--- lib/db/dao/visit_reason_dao.dart
+++ lib/db/dao/visit_reason_dao.dart
@@ -17,8 +17,8 @@ class VisitReasonDao extends DatabaseAccessor<MyDatabase>
       into(visitReasons).insert(visitReason);
 
   // [追加：複数]
-  addAllVisitReasons(List<VisitReason> visitReasonList){
-    batch((batch){
+  addAllVisitReasons(List<VisitReason> visitReasonList) {
+    batch((batch) {
       batch.insertAll(visitReasons, visitReasonList);
     });
   }
@@ -27,14 +27,15 @@ class VisitReasonDao extends DatabaseAccessor<MyDatabase>
   get allVisitReasons => select(visitReasons).get();
 
   // [削除：１件]
-  deleteVisitReason(VisitReason visitReason) => (delete(visitReasons)..where((t) => t.id.equals(visitReason.id))).go();
+  deleteVisitReason(VisitReason visitReason) =>
+      (delete(visitReasons)..where((t) => t.id.equals(visitReason.id))).go();
 
   //
   // -- トランザクション処理 --
   //
 
   // [追加：１件追加 -> 全取得]
-  addAndGetAllVisitReasons(VisitReason visitReason){
+  addAndGetAllVisitReasons(VisitReason visitReason) {
     return transaction(() async {
       await addVisitReason(visitReason);
       return await allVisitReasons;
@@ -42,7 +43,7 @@ class VisitReasonDao extends DatabaseAccessor<MyDatabase>
   }
 
   // [追加：複数追加 -> 全取得]
-  addAllAndGetAllVisitReasons(List<VisitReason> visitReasonList){
+  addAllAndGetAllVisitReasons(List<VisitReason> visitReasonList) {
     return transaction(() async {
       await addAllVisitReasons(visitReasonList);
       return await allVisitReasons;
@@ -50,12 +51,10 @@ class VisitReasonDao extends DatabaseAccessor<MyDatabase>
   }
 
   // [削除：１件削除 -> 全取得]
-  deleteAndGetAllVisitReasons(VisitReason visitReason){
+  deleteAndGetAllVisitReasons(VisitReason visitReason) {
     return transaction(() async {
       await deleteVisitReason(visitReason);
       return await allVisitReasons;
     });
   }
-
-
 }
