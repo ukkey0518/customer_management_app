be4f39d0060c3c3cd3aa0db54cd1e3de36b01bdd
ukkey0518 <ukitto0518@gmail.com>
2020-05-16 15:44:33 +0900

    [Provider] 追加：GlobalRepositoryの登録

--- lib/di/provider_repositories.dart
+++ lib/di/provider_repositories.dart
@@ -5,6 +5,7 @@ import 'package:customermanagementapp/db/dao/menu_dao.dart';
 import 'package:customermanagementapp/db/dao/visit_history_dao.dart';
 import 'package:customermanagementapp/repositories/customer_repository.dart';
 import 'package:customermanagementapp/repositories/employee_repository.dart';
+import 'package:customermanagementapp/repositories/global_repository.dart';
 import 'package:customermanagementapp/repositories/menu_category_repository.dart';
 import 'package:customermanagementapp/repositories/menu_repository.dart';
 import 'package:customermanagementapp/repositories/menus_by_category_repository.dart';
@@ -80,5 +81,24 @@ List<SingleChildWidget> depRepProviders = [
     ),
     update: (_, mRep, mcRep, mbcRep) =>
         mbcRep..onRepositoriesUpdated(mRep, mcRep),
+  ),
+  // [Rep: GlobalRepository]
+  // (依存) CustomerRepository, EmployeeRepository, MenuRepository, MenuCategoryRepository, VisitHistoryRepository
+  ChangeNotifierProxyProvider5<
+      CustomerRepository,
+      EmployeeRepository,
+      MenuCategoryRepository,
+      MenuRepository,
+      VisitHistoryRepository,
+      GlobalRepository>(
+    create: (context) => GlobalRepository(
+      cRep: Provider.of<CustomerRepository>(context, listen: false),
+      eRep: Provider.of<EmployeeRepository>(context, listen: false),
+      mcRep: Provider.of<MenuCategoryRepository>(context, listen: false),
+      mRep: Provider.of<MenuRepository>(context, listen: false),
+      vhRep: Provider.of<VisitHistoryRepository>(context, listen: false),
+    ),
+    update: (_, cRep, eRep, mcRep, mRep, vhRep, globalRep) =>
+        globalRep..onRepositoriesUpdated(cRep, eRep, mcRep, mRep, vhRep),
   ),
 ];
