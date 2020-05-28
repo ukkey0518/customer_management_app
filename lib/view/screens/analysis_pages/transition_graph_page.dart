135f93ff6e99971240cb02b798c7fd90dd9b49d1
ukkey0518 <ukitto0518@gmail.com>
2020-05-28 21:51:25 +0900

    [推移グラフ画面] 修正：スクロール可能に修正。タイトル追加

--- lib/view/screens/analysis_pages/transition_graph_page.dart
+++ lib/view/screens/analysis_pages/transition_graph_page.dart
@@ -25,34 +25,45 @@ class _TransitionGraphPageState extends State<TransitionGraphPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            PageSelectTabs(
-              tabs: _modeTabs,
-              selectedValue: _selectedMode,
-              onChanged: (value) {
-                setState(() {
-                  _selectedMode = value;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: AspectRatio(
-                  aspectRatio: 1.20,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          PageSelectTabs(
+            tabs: _modeTabs,
+            selectedValue: _selectedMode,
+            onChanged: (value) {
+              setState(() {
+                _selectedMode = value;
+              });
+            },
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Card(
                   child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: LineChart(_chartData()),
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '総来店数',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        SizedBox(height: 8),
+                        AspectRatio(
+                          aspectRatio: 1.20,
+                          child: LineChart(_chartData()),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
