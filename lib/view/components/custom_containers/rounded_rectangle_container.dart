7789806f79a0bcad8bd714d5eb14649cdcb3b218
ukkey0518 <ukitto0518@gmail.com>
2020-06-04 10:10:59 +0900

    [Component] 修正：RoundedRectangleContainerへheightとwidthパラメータを追加

--- lib/view/components/custom_containers/rounded_rectangle_container.dart
+++ lib/view/components/custom_containers/rounded_rectangle_container.dart
@@ -7,6 +7,8 @@ class RoundedRectangleContainer extends StatelessWidget {
     this.color,
     this.borderColor = Colors.grey,
     this.borderWidth = 1.0,
+    this.height,
+    this.width,
   })  : assert(radius != null),
         assert(borderWidth != null);
 
@@ -15,10 +17,14 @@ class RoundedRectangleContainer extends StatelessWidget {
   final Color color;
   final Color borderColor;
   final double borderWidth;
+  final double height;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: height,
+      width: width,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(radius)),
         border: Border.all(color: borderColor, width: borderWidth),
