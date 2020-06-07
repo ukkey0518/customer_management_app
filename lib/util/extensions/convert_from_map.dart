f717a741964f7e64405e4cb0847426c8183a8d57
ukkey0518 <ukitto0518@gmail.com>
2020-06-07 12:20:19 +0900

    [拡張: Map<K, V>] 修正：valueからkeyを取得する処理で、Valueが複数存在する場合に例外を投げるように設定

--- lib/util/extensions/convert_from_map.dart
+++ lib/util/extensions/convert_from_map.dart
@@ -1,9 +1,21 @@
 extension ConvertFromMap<K, V> on Map<K, V> {
   // [取得：valueからkeyを取得]
+  // ・valueが存在しない場合はnullを返す
+  // ・valueが複数存在する場合は例外発生
   K getKeyFromValue(V value) {
     final entries = this.entries.toList();
-    final key = entries.where((entry) => entry.value == value);
-    final returnKey = key.isEmpty ? null : key.single.key;
-    return returnKey;
+    var keyList = entries.where((entry) => entry.value == value);
+    var key;
+    switch (keyList.length) {
+      case 0:
+        key = null;
+        break;
+      case 1:
+        key = keyList.single;
+        break;
+      default:
+        throw Exception('Valueに一致するKeyが複数存在');
+    }
+    return key;
   }
 }
