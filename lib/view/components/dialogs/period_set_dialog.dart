de983cb14162824e7522e8cc497bb8c3d0496f87
ukkey0518 <ukitto0518@gmail.com>
2020-06-08 18:31:05 +0900

    [Components: date_select_buttons_column.dart] 修正：ディレクトリ移動

--- lib/view/components/dialogs/period_set_dialog.dart
+++ lib/view/components/dialogs/period_set_dialog.dart
@@ -2,7 +2,7 @@ import 'package:customermanagementapp/data/enums/periodMode.dart';
 import 'package:customermanagementapp/data/period_select_mode_text_entry.dart';
 import 'package:customermanagementapp/util/extensions/extensions.dart';
 import 'package:customermanagementapp/view/components/dialogs/dialog_title_text.dart';
-import 'package:customermanagementapp/view/components/date_select_buttons_column.dart';
+import 'package:customermanagementapp/view/components/custom_column/date_select_buttons_column.dart';
 import 'package:customermanagementapp/view/components/buttons/raised_switch_buttons.dart';
 import 'package:flutter/material.dart';
 
